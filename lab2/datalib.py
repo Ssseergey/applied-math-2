7d069f0765af27b96048d6c4bacac5011dd20e6c
Sergey Tolmachev <mr.tolmse121@gmail.com>
2022-11-30 15:25:05 +0000

    lab2 added

---
+++ lab2/datalib.py
@@ -0,0 +1,140 @@
+import numpy as np
+
+class PrepareData:
+    def __init__(self):
+        self.max_var = 0
+
+    def readTest(self, filename):
+        self.max_var = 0
+        self.max_x = 0
+        self.testname = filename
+        with open(filename) as f:
+            lines = f.readlines()
+            for i in range(len(lines)):
+                lines[i] = lines[i].rstrip()
+
+            self.type = self.parseType(lines[0])
+            self.function = self.parseFunction(lines[1])
+            self.equations = self.parseEquations(lines[2:])
+
+            self.A = self.createA()
+            self.b = self.createB()
+        return self.A, self.b
+
+    def parseType(self, type):
+        return type != 'min'
+
+    def parseFunction(self, function):
+        function_variables_with_coeff = function.split(' ')[2:]
+        parsed = self.parseCoeffsAndNumbers(function_variables_with_coeff)
+        for key, value in parsed.items():
+            parsed[key] = int(value)
+        return parsed
+
+    def parseCoeffsAndNumbers(self, line):
+        parsed = {}
+        for item in line:
+            coeff, var_number = item.split('x')
+            if not coeff:
+                parsed['x' + var_number] = 1
+            elif coeff == '-':
+                parsed['x' + var_number] = -1
+            else:
+                parsed['x' + var_number] = int(coeff)
+
+            self.max_var = max(self.max_var, int(var_number))
+        return parsed
+
+    def parseEquations(self, equations):
+        parsed = []
+        pre_parsed, b, type = [], [], []
+        for item in equations:
+            pre_parsed_part, b_part, type_part = self.parseEquation(item)
+            pre_parsed.append(pre_parsed_part)
+            b.append(b_part)
+            type.append(type_part)
+
+        self.max_x = self.max_var
+        for i in range(len(equations)):
+            parsed.append(self.convertToCanonical(pre_parsed[i], b[i], type[i]))
+        return parsed
+
+    def parseEquation(self, equation):
+        equation = equation.split('=')
+        type = '=' if equation[0][-1] == ' ' else equation[0][-1] + '='
+        b = int(equation[1])
+
+        equation = equation[0][:-len(type)].split(' ')
+        parsed = self.parseCoeffsAndNumbers(equation)
+
+        return parsed, b, type
+
+    def convertToCanonical(self, parsed, b, type):
+        if type == '=':
+            if b < 0:
+                # print(parsed)
+                b = -b
+                for key, value in parsed.items():
+                    parsed[key] = -value
+            return parsed, b
+
+        parsed['x' + str(self.max_var + 1)] = -1 if type == '>=' else 1
+
+        if b < 0:
+            b = -b
+            for key, value in parsed:
+                parsed[key] = -value
+
+        self.max_var += 1
+        return parsed, b
+
+    def print(self):
+        print("Test name: ", self.testname)
+        print("We want find", 'MAX' if self.type else 'MIN')
+        print("Function is ", end='')
+        for i, (key, value) in enumerate(self.function.items()):
+            if i > 0:
+                print('+ ', end='')
+            print(('' if value == 1 else str(value)) + key + ' ', end='')
+        print()
+        print("Equations:")
+        for equ in self.equations:
+            for i, (key, value) in enumerate(equ[0].items()):
+                if i > 0:
+                    print('+ ', end='')
+                print(('' if value == 1 else str(value)) + key + ' ', end='')
+            print('=', equ[1])
+        print("A")
+        print(self.A)
+        print("b")
+        print(self.b)
+
+    def createA(self):
+        A = []
+        for equation in self.equations:
+            line = []
+            for i in range(1, self.max_var + 1):
+                value = 0
+                try:
+                    value = equation[0]['x' + str(i)]
+                except:
+                    pass
+                line.append(value)
+            A.append(line)
+        line = []
+        for i in range(1, self.max_var + 1):
+            value = 0
+            try:
+                value = self.function['x' + str(i)]
+            except:
+                pass
+            line.append(value)
+        A.append(line)
+        return np.array(A, dtype=np.float64)
+
+    def createB(self):
+        b = []
+        for item in self.equations:
+            b.append(item[1])
+        b.append(0)
+        return np.array(b, dtype=np.float64)
---
+++ lab2/main.py
@@ -0,0 +1,92 @@
+import datalib as dl
+import solver
+from colorama import Fore, Style
+
+def countPossibilities(game_cost, values):
+    pi = []
+    for i in range(len(values)):
+        try:
+            # print("P[", i, "] = ", game_cost * values[str(i)])
+            pi.append(game_cost * values[str(i)])
+        except:
+            # print("P[", i, "] = ", 0)
+            pi.append(0)
+
+    return pi
+
+def countM(A, P, Q):
+    M = 0
+    for i in range(len(A) - 1):
+        for j in range(len(P)):
+            try:
+                M += A[i][j] * P[i] * Q[j]
+            except:
+                pass
+    return M
+
+
+data = dl.PrepareData()
+
+
+lineral_tests = {
+    "task2": 298,
+    "task3": 480000,
+}
+
+matrix_tests = {
+    "task4": 0.75,
+    "task5": 0.5,
+    "task6": 0.28,
+}
+
+
+for test, value in lineral_tests.items():
+    data.readTest("tests/" + test)
+    worker = solver.Solver(data.A, data.b, data.type, data.max_x, data.max_var, False)
+    result = worker.solve()
+    if round(result, 2) == value:
+        print(Fore.GREEN + test + " Passed!")
+    else:
+        print(Fore.RED + test + " Failed!")
+    print(Style.RESET_ALL)
+
+
+for test, value in matrix_tests.items():
+    data.readTest("tests/" + test + "1")
+    worker = solver.Solver(data.A, data.b, data.type, data.max_x, data.max_var, True)
+    resultPlayer1, valuesPlayer1 = worker.solve()
+
+    data.readTest("tests/" + test + "2")
+    worker = solver.Solver(data.A, data.b, data.type, data.max_x, data.max_var, True)
+    result, valuesPlayer2 = worker.solve()
+
+    P = countPossibilities(1 / resultPlayer1, valuesPlayer2)
+    Q = countPossibilities(1 / resultPlayer1, valuesPlayer1)
+    print("Possibilities for player1 = ", P)
+    print("Possibilities for player2 = ", Q)
+
+    M = countM(data.A, P, Q)
+    print("M = ", M)
+
+    if round(result, 2) == value:
+        print(Fore.GREEN + test + " Passed!")
+    else:
+        print(Fore.RED + test + " Failed!")
+    print(Style.RESET_ALL)
+
+
+# A = [
+#     [1, -1, -1],
+#     [-1, -1, 3],
+#     [-1, 2, -1]
+# ]
+
+# P = [6/13, 3/13, 4/13]
+# Q = [6/13, 4/13, 3/13]
+
+
+# sum = 0
+# for i in range(len(A)):
+#     for j in range(len(A[0])):
+#         sum += P[i] * Q[j] * A[i][j]
+# print(sum)
